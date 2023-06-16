a52d849e99113b1c1b84d867158cc0a1e083b956
Daniel Enabulele <89023177+Draco247@users.noreply.github.com>
2023-06-16 12:32:17 +0100

    quests
    updated quest section

--- scraper.py
+++ scraper.py
@@ -1,71 +1,205 @@
 import csv
 import urllib.request
-#from PIL import Image
+from PIL import Image
 from bs4 import BeautifulSoup
 import requests
 import pandas as pd
-
+from io import BytesIO
+import mysql.connector
 headers = ({'User-Agent':
                 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.157 Safari/537.36',
             'Accept-Language': 'en-US, en;q=0.5'})
 base_url = "https://mhrise.kiranico.com/"
+mydb = mysql.connector.connect(
+            host="localhost",
+            user="root",
+            password="ShadowSlash247",
+            database="monster_hunter"
+        )
 
 
 class Scraper(object):
 
-    def __init__(self, headers, base_url):
+    def __init__(self, headers, base_url, mydb):
         self.headers = headers
         self.base_url = base_url
+        self.mydb = mydb
 
     def get_monster(self):
         url = f"{base_url}data/monsters?view=lg"
         session = requests.Session()
         r = session.get(url, headers=headers)
         soup = BeautifulSoup(r.content, "html.parser")
+        # print(soup)
         self.get_monster_pages(soup)
 
     def get_monster_pages(self, soup):
         all_monsters = soup.find_all("div", attrs={"class": "group relative p-4 border-r border-b border-gray-200 dark:border-gray-800 sm:p-6"})
         # print(all_monsters)
-        for monster in all_monsters:
+        for monster in all_monsters[1:2]:
             # print(monster)
-            monster_name = monster.find('a')
+            monster_name = monster.find('a') 
             monster_name = monster_name.text.strip()
             monster_img = monster.find('img')["src"]
             link = monster.find('a')['href']
+            monster_id = link.split("monsters/")[1]
             print(monster_name)
-            print(link)
-            print(monster_img)
+            # print(link)
+            # print(monster_img)
+            # print(monster_id)
+            # sql = "INSERT INTO monsters (name, link, image_link, monster_id) VALUES (%s,%s,%s,%s)"
+            # val = ([monster_name,link,monster_img, monster_id])
+            # mycursor = mydb.cursor()
+            # # print(mycursor)
+            # mycursor.execute(sql,val)
+            # # print(mycursor)
+            
+            # response = requests.get(monster_img)
+            # image = Image.open(BytesIO(response.content))
+            # image.show()
             session = requests.Session()
             r = session.get(link, headers=headers)
             soup = BeautifulSoup(r.content, "html.parser")
-            print(self.get_monster_details(soup))
+            print(self.get_monster_details(soup, monster_id))
             # if monster_name == "Gold Rathian":
             #     print(monster_name)
                 
                 # print(monster_name)
+        mydb.commit()
 
-    def get_monster_details(self, soup):
-        monster_drop_table = soup.find_all('table')[5]
-        row = monster_drop_table.find_all('tr')
-        count = 0
-        for item in row:
-            item_name = item.find('a').text
-            quest_rank = item.find_all("td")[1].text
-            method = item.find_all("td")[2].text
-            area = item.find_all("td")[3].text
-            amount = item.find_all("td")[4].text
-            rate = item.find_all("td")[5].text
-            print(item_name)
-            print(quest_rank)
-            print(method)
-            print(area)
-            print(amount)
-            print(rate)
-            monster_item = {"Monster Part": item_name, "Monster Part Rank": quest_rank}
-            print(monster_item)
-            print("----------")
+    def get_monster_details(self, soup, monster_id):
+        # hitzones = []
+        # get_hitzones = self.get_monster_physiology(soup, hitzones)
+        # drops = []
+        # get_drops = self.get_monster_drops(soup, drops)
+        quests = []
+        get_quests = self.get_monster_quests(soup,quests, monster_id)
+        # print(hitzones)
+        # print(drops)
+        # print(quests)
+        # self.get_monster_drops(soup)
+
+    def get_monster_physiology(self, soup, hitzones):
+        monster_hitzones = soup.find_all('table')[0]# find monster phys table on page
+        rows = monster_hitzones.find_all('tr')
+        for row in rows:# extract all important details from the table such as hitzone values for the different weapon types
+            hitzone = row.find_all('td')[0].text
+            hitzone_state = row.find_all('td')[1].text
+            blade_hitzone_value = row.find_all('td')[2].text
+            blunt_hitzone_value = row.find_all('td')[3].text
+            gunner_hitzone_value = row.find_all('td')[4].text
+            fire_hitzone_value = row.find_all('td')[5].text
+            water_hitzone_value = row.find_all('td')[6].text
+            ice_hitzone_value = row.find_all('td')[7].text
+            thunder_hitzone_value = row.find_all('td')[8].text
+            dragon_hitzone_value = row.find_all('td')[9].text
+            stun_hitzone_value = row.find_all('td')[10].text
+            hitzones.append({'hitzone': hitzone, 'state': hitzone_state, 'blade hitzone': blade_hitzone_value, 'blunt hitzone': blunt_hitzone_value,
+                             'gunner hitzone': gunner_hitzone_value, 'fire hitzone': fire_hitzone_value, 'water hitzone': water_hitzone_value,
+                              'ice hitzone': ice_hitzone_value, 'thunder hitzone': thunder_hitzone_value, 'dragon hitzone': dragon_hitzone_value,
+                               'stun': stun_hitzone_value})
+        return hitzones
+        # return monster_hitzones
+
+    def get_monster_drops(self, soup, parts):
+        monster_drop_table = soup.find_all('table')[5] # find monster drop/carves table on page
+        rows = monster_drop_table.find_all('tr')
+        for row in rows:# extract all useful information such as the item name, where it can be dropped from, percentage, etc
+            item_name = row.find('a').text
+            quest_rank = row.find_all("td")[1].text
+            method = row.find_all("td")[2].text
+            area = row.find_all("td")[3].text
+            amount = row.find_all("td")[4].text
+            rate = row.find_all("td")[5].text
+            # print(item_name)
+            # print(quest_rank)
+            # print(method)
+            # print(area)
+            # print(amount)
+            # print(rate)
+            monster_item = {"Monster Part": item_name, "Monster Part Rank": quest_rank , "Drop Method": method, "Drop Area": area,
+                            "Amount": amount, "Drop Rate": rate}
+            parts.append(monster_item)
+        return parts
+            # print(monster_item)
+            # print("----------")
         # print(monster_drop_table)
+
+    def get_monster_quests(self, soup, quests, monster_id):
+        quest_table = soup.find_all("table")[4]
+        rows = quest_table.find_all("tr")
+        for row in rows[0:1]:
+            quest_name = row.find("a").text
+            quest_link = row.find('a')['href']
+            quest_id = quest_link.split("quests/")[1]
+            print(quest_link)
+            print(quest_id)
+            session = requests.Session()
+            r = session.get(quest_link, headers=headers)
+            soup = BeautifulSoup(r.content, "html.parser")
+            quest_objective = soup.find_all("div", attrs={"class": "px-4 py-5 sm:p-6"})[0].text.split("Objective")[1].split("(")[0].strip()
+            print(quest_objective)
+            hunter_rank_points = soup.find_all("div", attrs={"class": "px-4 py-5 sm:p-6"})[0].text.split("Objective")[1].split("HRP:")[1].split("pts")[0].strip()
+            print(hunter_rank_points)
+            master_rank_points = soup.find_all("div", attrs={"class": "px-4 py-5 sm:p-6"})[0].text.split("Objective")[1].split("MRP:")[1].split("pts")[0].strip()
+            print(master_rank_points)
+            failure_conditions = soup.find_all("div", attrs={"class": "px-4 py-5 sm:p-6"})[1].text.split("Conditions")[1].strip()
+            print(failure_conditions)
+
+            # sizes_table = soup.find_all("div", attrs={"class": "basis-1/5"})
+            sizes_table = soup.find(lambda tag: tag.name == "div" and
+                                   "basis-1/5" in tag.get("class", []) and
+                                   tag.find("a", href=lambda href: monster_id in href)).find("table")
+
+
+            # print(sizes_table)
+            mini_crown_chances = []
+            king_crown_chances = []
+            mini_crowns = sizes_table.select(f'tr:has(img[src*="crown_mini"])')
+            # print(mini_crowns)
+            for mini_crown in mini_crowns:
+                size = mini_crown.text.strip().split(" ")[0].replace("\n", "")
+                chance = mini_crown.text.strip().split(" ")[-1].replace("\n", "")
+                mini_crown_chances.append({size:chance})
+            print(mini_crown_chances)
+            king_crowns = sizes_table.select(f'tr:has(img[src*="crown_king"])')
+            for king_crown in king_crowns:
+                size = king_crown.text.strip().split(" ")[0].replace("\n", "")
+                chance = king_crown.text.strip().split(" ")[-1].replace("\n", "")
+                king_crown_chances.append({size:chance})
+            print(king_crown_chances)
+
+            quest_rewards_table = soup.find(lambda tag: tag.name == "div" and
+                                   "basis-1/2" in tag.get("class", [])).find("table")
+            # print(quest_rewards_table)
+
+            rewards_rows = quest_rewards_table.find_all("tr")
+            for reward_row in rewards_rows:
+                # print(reward_row)
+                item_name = reward_row.find("a").text
+                print(item_name)
+                quantity = reward_row.find_all("td")[2].text
+                print(quantity)
+                reward_chance = reward_row.find_all("td")[3].text
+                print(reward_chance)
+                
+            # selected_monster_rows = sizes_table.select(f'a[href*="{monster_id}"]')#need to get mini and king crown sizes
+            # print(selected_monster_rows)
+        #     gold_crowns = row.find_all("td")[0].find_all("div")[-1]
+        #     # quest_areas = []
+        #     gold_crown_chances = gold_crowns.text.strip().replace(" ", "").split()
+        #     # num_players = row.find_all("td")[1].text
+        #     monster_HP_values = row.find_all("td")[2].find_all("div")
+        #     monster_HP = []
+        #     for value in monster_HP_values:
+        #         monster_HP.append(value.text)
+        #     # print(monster_HP)
+        #     monster_quests = {"Quest Name" : quest_name, "Gold Crown Chances": gold_crown_chances, "Monster HP" : monster_HP}
+        #     quests.append(monster_quests)
+        # return quests
+
+
+   
 
 
     def get_all_weapon(self):
@@ -254,6 +388,6 @@ class Scraper(object):
             # print(wpn_name)
 
 
-webscrape = Scraper(headers, base_url)
+webscrape = Scraper(headers, base_url,mydb)
 # webscrape.get_all_weapon()
 webscrape.get_monster()
