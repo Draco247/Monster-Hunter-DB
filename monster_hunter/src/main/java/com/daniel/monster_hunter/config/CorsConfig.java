bcd9d7368a962722c782e9fb6d8a19daab738579
Draco247 <denabulele25@gmail.com>
2023-09-03 22:32:23 +0100

    config was in wrong location

--- monster_hunter/src/main/java/com/daniel/monster_hunter/config/CorsConfig.java
+++ monster_hunter/src/main/java/com/daniel/monster_hunter/config/CorsConfig.java
@@ -1,4 +1,4 @@
-package com.daniel.config;
+package com.daniel.monster_hunter.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
